69bce968483d054027a7602a3988183f251e6bc0
yes <romain.picoche@gmail.com>
2023-10-08 16:13:47 +0200

    V1

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <sys/types.h>
+#include <unistd.h>
+#include <signal.h>
+
+void capout(int a)
+{
+  write(1,"HAHA...NO!!\n", 12);
+}
+int main(void)
+{
+  signal(SIGINT,capout);
+  while (1)
+    {
+      //      fork();
+    }
+}
